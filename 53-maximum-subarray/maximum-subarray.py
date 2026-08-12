675af4ff28863bcef234a23584a84c6407088696
Dk <dhruvkachhela2006@gmail.com>
2026-08-13 00:23:16 +0530

    Time: 95 ms (47.30%) | Memory: 21.4 MB (25.84%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -5,10 +5,9 @@ class Solution(object):
         :rtype: int
         """
         current_sum = nums[0]
-        max_sum = nums[0]
-
-        for i in range(1, len(nums)):
-            current_sum = max(nums[i], current_sum + nums[i])
-            max_sum = max(max_sum, current_sum)
-
+        max_sum = current_sum
+        
+        for i in range( 1 , len(nums)):
+            current_sum = max(nums[i] , current_sum + nums[i])
+            max_sum = max(max_sum , current_sum)
         return max_sum
