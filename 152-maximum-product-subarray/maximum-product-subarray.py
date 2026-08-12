764aba25ee561c3fff42ed170d64ec9c491b8e74
Dk <dhruvkachhela2006@gmail.com>
2026-08-13 00:34:55 +0530

    Time: 12 ms (31.11%) | Memory: 13.4 MB (6.83%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_ending =min_ending = res = nums[0]
+
+        for i in range(1 ,len(nums)):
+            v1 = nums[i]
+            v2 = max_ending*nums[i]
+            v3 = min_ending*nums[i]
+            max_ending = max(v1 , max(v2,v3))
+            min_ending = min ( v1 , min(v2 , v3))
+            res = max( res , max(max_ending , min_ending))
+        return res
