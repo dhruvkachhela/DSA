45c7206763a7e9da81e00085ebcd3dd02cc5573b
Dk <dhruvkachhela2006@gmail.com>
2026-08-30 18:35:46 +0530

    Time: 15 ms (85.99%) | Memory: 12.5 MB (66.97%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) == len(t):
+            seen= {}
+            for char in s:
+                seen[char] = seen.get(char , 0)+1
+
+            for char in t:
+                if char not in seen:
+                    return False
+                seen[char] -=1
+
+                if seen[char] ==0:
+                    del seen[char]
+            return True
+
+        else:
+            return False
+
+
+        
