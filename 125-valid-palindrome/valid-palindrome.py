6329fca7e87e7ce7a8a6b571dfde2d6f3bdc1b3f
Dk <dhruvkachhela2006@gmail.com>
2026-08-30 18:46:10 +0530

    Time: 18 ms (49.85%) | Memory: 14.1 MB (16.51%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s =''.join(char.lower() for char in s if char.isalnum())
+
+        left = 0
+        right = len(s)-1
+        while left<right:
+            if s[left] != s[right]:
+                return False
+            left +=1
+            right -=1
+        return True
